5b0c072828992eb397673e1420f805ce04702a29
TicklishReboots <67234652+TicklishReboots@users.noreply.github.com>
2022-07-12 10:16:13 +0200

    last requirement done

--- src/features/topics/Topic.js
+++ src/features/topics/Topic.js
@@ -1,14 +1,16 @@
 import NewTopicForm from "../../components/NewTopicForm";
 import { Link, useParams } from "react-router-dom";
 import ROUTES from "../../app/routes";
+import { useSelector } from "react-redux";
+import { selectTopics } from "./topicsSlice";
+import { selectQuizzes } from "../quizzes/quizzesSlice";
 
 export default function Topic() {
-  const topics = {}; // replace this with a call to your selector to select all the topics in state
-  const quizzes = {}; // replace this with a call to your selector to select all the quizzes in state
+  const topics = useSelector(selectTopics); // replace this with a call to your selector to select all the topics in state
+  const quizzes = useSelector(selectQuizzes); // replace this with a call to your selector to select all the quizzes in state
   let { topicId } = useParams();
   const topic = topics[topicId];
-  console.log(topics);
-  const quizzesForTopic = topic.quizIds ? topic.quizIds.map((quizId) => quizzes[quizId]) : null;
+  const quizzesForTopic = topic.quizIds.map((quizId) => quizzes[quizId]);
 
   if (!topic) {
     return <div className="404">404</div>
